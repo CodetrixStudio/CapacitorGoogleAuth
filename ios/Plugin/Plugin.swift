499c296e04005015de98d51a2d023de88333cb0d
Chris Trengove <trengove@econdata.com.au>
2020-03-19 15:35:25 +1100

    Update GoogleSignin to version 5, primarily to fix the dependency on the deprecated UIWebView API. Bump the plug-in version to 1.5.1.

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -10,40 +10,40 @@ import GoogleSignIn
 public class GoogleAuth: CAPPlugin {
     var signInCall: CAPPluginCall?
     let googleSignIn: GIDSignIn = GIDSignIn.sharedInstance();
-    
+
     public override func load() {
         guard let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") else {return}
         guard let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {return}
         guard let clientId = dict["CLIENT_ID"] as? String else {return}
-        
+
         googleSignIn.clientID = clientId;
         googleSignIn.delegate = self;
-        googleSignIn.uiDelegate = self;
-        
+        googleSignIn.presentingViewController = bridge.viewController;
+
         if let serverClientId = getConfigValue("serverClientId") as? String {
             googleSignIn.serverClientID = serverClientId;
         }
-        
+
         if let scopes = getConfigValue("scopes") as? [String] {
             googleSignIn.scopes = scopes;
         }
-        
+
         NotificationCenter.default.addObserver(self, selector: #selector(handleOpenUrl(_ :)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil);
     }
-    
+
     @objc
     func signIn(_ call: CAPPluginCall) {
         signInCall = call;
-        
+
         DispatchQueue.main.async {
-            if self.googleSignIn.hasAuthInKeychain() {
-                self.googleSignIn.signInSilently();
+            if self.googleSignIn.hasPreviousSignIn() {
+                self.googleSignIn.restorePreviousSignIn();
             } else {
                 self.googleSignIn.signIn();
             }
         }
     }
-    
+
     @objc
     func refresh(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
@@ -51,13 +51,13 @@ public class GoogleAuth: CAPPlugin {
                 call.error("User not logged in.");
                 return
             }
-            
+
             self.googleSignIn.currentUser.authentication.getTokensWithHandler { (authentication, error) in
                 guard let authentication = authentication else {
                     call.error(error?.localizedDescription ?? "Something went wrong.");
                     return;
                 }
-                
+
                 let authenticationData: [String: Any] = [
                     "accessToken": authentication.accessToken,
                     "idToken": authentication.idToken,
@@ -75,28 +75,22 @@ public class GoogleAuth: CAPPlugin {
         }
         call.success();
     }
-    
+
     @objc
     func handleOpenUrl(_ notification: Notification) {
         guard let object = notification.object as? [String: Any] else {
             print("There is no object on handleOpenUrl");
             return;
         }
-        
+
         guard let url = object["url"] as? URL else {
             print("There is no url on handleOpenUrl");
             return;
         }
-        
-        guard let options = object["options"] as? [UIApplication.OpenURLOptionsKey : Any] else {
-            print("There is no options on handleOpenUrl");
-            return;
-        }
-        
-        let sourceApplication = options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String;
-        googleSignIn.handle(url, sourceApplication: sourceApplication, annotation: [:]);
+
+        googleSignIn.handle(url);
     }
-    
+
     func processCallback(user: GIDGoogleUser) {
         var userData: [String: Any] = [
             "authentication": [
@@ -111,11 +105,11 @@ public class GoogleAuth: CAPPlugin {
             "id": user.userID,
             "name": user.profile.name
         ];
-        
+
         if let imageUrl = user.profile.imageURL(withDimension: 100)?.absoluteString {
             userData["imageUrl"] = imageUrl;
         }
-        
+
         signInCall?.success(userData);
     }
 }
@@ -126,17 +120,7 @@ extension GoogleAuth: GIDSignInDelegate {
             signInCall?.error(error.localizedDescription);
             return;
         }
-        
-        processCallback(user: user);
-    }
-}
 
-extension GoogleAuth: GIDSignInUIDelegate {
-    public func sign(_ signIn: GIDSignIn!, present viewController: UIViewController!) {
-        bridge.viewController.present(viewController, animated: true);
-    }
-    
-    public func sign(_ signIn: GIDSignIn!, dismiss viewController: UIViewController!) {
-        viewController.dismiss(animated: true);
+        processCallback(user: user);
     }
 }
