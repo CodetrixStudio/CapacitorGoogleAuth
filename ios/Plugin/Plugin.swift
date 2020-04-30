d0912cbd51cafd8d63970990ce3311e582fc608a
Thibaud <thibaud.sanchez@emvista.com>
2020-04-30 18:18:20 +0200

    Fix line feed ios (#3)
    Fix line feed ios for 2.1.1

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,7 +1,7 @@
 import Foundation
 import Capacitor
 import GoogleSignIn
-​
+
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
@@ -16,30 +16,24 @@ public class GoogleAuth: CAPPlugin {
         guard let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") else {return}
         guard let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {return}
         guard let clientId = dict["CLIENT_ID"] as? String else {return}
-​
         googleSignIn.clientID = clientId;
         googleSignIn.delegate = self;
         googleSignIn.presentingViewController = bridge.viewController;
-​
         if let serverClientId = getConfigValue("serverClientId") as? String {
             googleSignIn.serverClientID = serverClientId;
         }
-​
         if let scopes = getConfigValue("scopes") as? [String] {
             googleSignIn.scopes = scopes;
         }
-​
         if let forceAuthCodeConfig = getConfigValue("forceCodeForRefreshToken") as? Bool {
             forceAuthCode = forceAuthCodeConfig;
         }
-        
         NotificationCenter.default.addObserver(self, selector: #selector(handleOpenUrl(_ :)), name: Notification.Name(CAPNotifications.URLOpen.name()), object: nil);
     }
-​
+    
     @objc
     func signIn(_ call: CAPPluginCall) {
         signInCall = call;
-​
         DispatchQueue.main.async {
             if self.googleSignIn.hasPreviousSignIn() && !self.forceAuthCode {
                 self.googleSignIn.restorePreviousSignIn();
@@ -48,7 +42,7 @@ public class GoogleAuth: CAPPlugin {
             }
         }
     }
-​
+    
     @objc
     func refresh(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
@@ -56,13 +50,11 @@ public class GoogleAuth: CAPPlugin {
                 call.error("User not logged in.");
                 return
             }
-​
             self.googleSignIn.currentUser.authentication.getTokensWithHandler { (authentication, error) in
                 guard let authentication = authentication else {
                     call.error(error?.localizedDescription ?? "Something went wrong.");
                     return;
                 }
-​
                 let authenticationData: [String: Any] = [
                     "accessToken": authentication.accessToken,
                     "idToken": authentication.idToken,
@@ -72,7 +64,7 @@ public class GoogleAuth: CAPPlugin {
             }
         }
     }
-​
+    
     @objc
     func signOut(_ call: CAPPluginCall) {
         DispatchQueue.main.async {
@@ -80,22 +72,20 @@ public class GoogleAuth: CAPPlugin {
         }
         call.success();
     }
-​
+    
     @objc
     func handleOpenUrl(_ notification: Notification) {
         guard let object = notification.object as? [String: Any] else {
             print("There is no object on handleOpenUrl");
             return;
         }
-​
         guard let url = object["url"] as? URL else {
             print("There is no url on handleOpenUrl");
             return;
         }
-​
         googleSignIn.handle(url);
     }
-​
+    
     func processCallback(user: GIDGoogleUser) {
         var userData: [String: Any] = [
             "authentication": [
@@ -110,22 +100,19 @@ public class GoogleAuth: CAPPlugin {
             "id": user.userID,
             "name": user.profile.name
         ];
-​
         if let imageUrl = user.profile.imageURL(withDimension: 100)?.absoluteString {
             userData["imageUrl"] = imageUrl;
         }
-​
         signInCall?.success(userData);
     }
 }
-​
+
 extension GoogleAuth: GIDSignInDelegate {
     public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if let error = error {
             signInCall?.error(error.localizedDescription);
             return;
         }
-​
         processCallback(user: user);
     }
 }
