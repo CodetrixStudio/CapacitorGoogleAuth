e520b4cecd9892a56cd79842d008fdde994ba06b
Patryk Szalanski <patryk.szalanski@gmail.com>
2020-11-27 07:41:19 +0100

    Make requesting server auth code optional (#68)
    * Make requesting server auth code optional, fixing https://github.com/CodetrixStudio/CapacitorGoogleAuth/issues/67
    
    * Update GoogleAuth.java
    
    Co-authored-by: Patryk Szalanski <patryk@eco-projects.global>

--- android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
+++ android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
@@ -27,7 +27,7 @@ public class GoogleAuth extends Plugin {
   @Override
   public void load() {
     String clientId = this.getContext().getString(R.string.server_client_id);
-    Boolean forceCodeForRefreshToken = false;
+    boolean forceCodeForRefreshToken = false;
 
     Boolean forceRefreshToken = (Boolean) getConfigValue("forceCodeForRefreshToken");
     if (forceRefreshToken != null) {
@@ -36,8 +36,11 @@ public class GoogleAuth extends Plugin {
 
     GoogleSignInOptions.Builder googleSignInBuilder = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(clientId)
-            .requestServerAuthCode(clientId, forceCodeForRefreshToken)
             .requestEmail();
+
+    if (forceCodeForRefreshToken) {
+      googleSignInBuilder.requestServerAuthCode(clientId, true);
+    }
 
     try {
       JSONArray scopeArray = (JSONArray) getConfigValue("scopes");
