ea6ed8282eef585f3d1d96cbf73bf29851aa394b
aerodynamics-digital <65594910+aerodynamics-digital@users.noreply.github.com>
2021-03-01 20:53:40 +0100

    Update web.ts

--- src/web.ts
+++ src/web.ts
@@ -10,11 +10,7 @@ export class GoogleAuthWeb extends WebPlugin implements GoogleAuthPlugin {
   gapiLoaded: Promise<void>;
 
   get webConfigured(): boolean {
-    if (typeof document === undefined) {
-      return false;
-    } else {
-      return document.getElementsByName('google-signin-client_id').length > 0;
-    }
+      return document?.getElementsByName('google-signin-client_id').length > 0;
   }
 
   constructor() {
