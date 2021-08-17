b887bc89cd346988323cd52de36a161954744263
reslear <reslear@gmail.com>
2021-08-17 13:27:57 +0300

    refactor: add android `init` unimplemented

--- android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
+++ android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
@@ -59,6 +59,11 @@ public class GoogleAuth extends Plugin {
     googleSignInClient = GoogleSignIn.getClient(this.getContext(), googleSignInOptions);
   }
 
+  @PluginMethod
+  public void init(PluginCall call) {
+      call.unimplemented();
+  }
+
   @PluginMethod()
   public void signIn(PluginCall call) {
     saveCall(call);
