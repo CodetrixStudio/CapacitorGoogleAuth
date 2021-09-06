789c6045b430b4e1f77839b42eff63a4801f7e50
reslear <reslear@gmail.com>
2021-09-06 13:57:00 +0300

    fix: set `initialize` CAP_PLUGIN_METHOD

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -7,4 +7,5 @@ CAP_PLUGIN(GoogleAuth, "GoogleAuth",
            CAP_PLUGIN_METHOD(signIn, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(refresh, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(signOut, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
 )
