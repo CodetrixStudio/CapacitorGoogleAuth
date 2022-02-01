585c8bd1820d1488ce08304eeb1a23516f4a324f
hoi4 <ph.heuer@googlemail.com>
2022-02-01 21:08:01 +0100

    refactor(android): separate messages for signin exception (#176)
    * added status code for android implementation
    
    * added iOS error code
    
    Co-authored-by: Philipp Heuer <philipp@studysmarter.de>

--- android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
+++ android/src/main/java/com/codetrixstudio/capacitor/GoogleAuth/GoogleAuth.java
@@ -43,6 +43,10 @@ public class GoogleAuth extends Plugin {
   private final static String FIELD_TOKEN_EXPIRES_IN = "expires_in";
   private final static String FIELD_ACCESS_TOKEN = "accessToken";
   private final static String FIELD_TOKEN_EXPIRES = "expires";
+
+  // see https://developers.google.com/android/reference/com/google/android/gms/auth/api/signin/GoogleSignInStatusCodes#SIGN_IN_CANCELLED
+  private final static int SIGN_IN_CANCELLED = 12501;
+
   public static final int KAssumeStaleTokenSec = 60;
 
   private GoogleSignInClient googleSignInClient;
@@ -121,7 +125,11 @@ public class GoogleAuth extends Plugin {
         }
       });
     } catch (ApiException e) {
-      call.reject("Something went wrong", e);
+      if (SIGN_IN_CANCELLED == e.getStatusCode()) {
+        call.reject("The user canceled the sign-in flow.", "" + e.getStatusCode());
+      } else {
+        call.reject("Something went wrong", "" + e.getStatusCode());
+      }
     }
   }
 
