56c4e6eb2337bf58f2c474b7195e761f3fd63d4d
reslear <reslear@gmail.com>
2024-04-29 02:07:58 +0200

    fix: revert iOS changes

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -1,5 +1,11 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin using the CAP_PLUGIN Macro
-CAP_PLUGIN(GoogleAuth, "GoogleAuth")
+// Define the plugin using the CAP_PLUGIN Macro, and
+// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
+CAP_PLUGIN(GoogleAuth, "GoogleAuth",
+           CAP_PLUGIN_METHOD(signIn, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(refresh, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(signOut, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
+)
